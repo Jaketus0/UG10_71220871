89348f1b21ea9c2c175cce633bb51c8d640ddd8a
Via Angelyca <117884184+Jaketus0@users.noreply.github.com>
2022-11-24 13:18:11 +0700

    Add files via upload

---
+++ 2_B_71220871.py
@@ -0,0 +1,8 @@
+print("="*10, "PROGRAM PENGHITUNG PYTHAGORAS","="*10)
+pert= int(input("Masukkan bilangan bulat pertama: "))
+ked= int(input("Masukkan bilangan bulat kedua: "))
+ket= int(input("Masukkan bilangan bulat ketiga: "))
+if((pert**2)+(ked**2)==(ket**2)):
+    print("Merupakan Pythagoras")
+else: 
+    print("Bukan merupakan Pythagoras")
